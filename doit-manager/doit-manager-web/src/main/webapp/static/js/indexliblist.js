0c82d5876a35aaf121f8bcce47d853880f0b6771
hituyuu <2256765706@qq.com>
2018-09-16 10:44:51 +0800

    点击加载按钮时,增加弹出层提示,以免用户再次点击加载按钮

--- doit-manager/doit-manager-web/src/main/webapp/static/js/indexliblist.js
+++ doit-manager/doit-manager-web/src/main/webapp/static/js/indexliblist.js
@@ -12,6 +12,7 @@ layui.use(['form', 'table', 'jquery', 'admin'], function () {
         //点击后禁用的代码自己完成
         $("#btnImport").attr("disabled", true);
 
+        var indexopen = layer.msg('正在加载,请稍后', {icon: 16,time: 0,shade: [0.5 , '#000' , true]});
         $.post(
             //url
             '../../item/indexlib/import',
@@ -23,6 +24,7 @@ layui.use(['form', 'table', 'jquery', 'admin'], function () {
                     console.log(data);
                     layer.msg(data.message)
                     $("#btnImport").attr("disabled", false);
+                    layer.close(indexopen);
                 }else{
                     console.log(data.message);
                     layer.msg(data.message)
